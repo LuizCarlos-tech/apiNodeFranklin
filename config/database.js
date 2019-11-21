516ffb36b743499d727d3130e5980b1cf5faf830
franklin-rodrigo <franklin@aluno.fapce.edu.br>
2019-11-21 19:02:06 -0300

    banco

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 module.exports = {
-  username: 'postgres',
-  password: 'postgres',
-  database: 'ws',
-  host: '127.0.0.1',
+  username: 'lqsdzsuircndpe',
+  password: 'e12e9ebe00cd98a932fa11a60ed7754e907df818fb628d38416dabe0b083a6a5',
+  database: 'd2q92pfe1mmj69',
+  host: 'ec2-174-129-229-162.compute-1.amazonaws.com',
   dialect: 'postgres',
+  port: 5432,
 }
